2fad84fc43ff8a31176651dc30da02b9b9397024
ran_dommm <791075814@qq.com>
2025-03-20 15:01:20 +0800

    add ignore

--- recognition/API_key.py
+++
@@ -1,6 +0,0 @@
-# Add your API key here
-# api_key_dict = {'silicon_flow_keys': [],
-#                 'DeepSeek-R1':[],
-#                 'DeepSeek-R1-671B':[],
-#                 'deepseek-chat':[]，
-#                 'deepseek-reasoner':[]，}
