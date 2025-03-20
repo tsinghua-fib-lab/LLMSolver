dcbd39b5e7b95d409fb5afa5792af81dc368d9e6
ran_dommm <791075814@qq.com>
2025-03-20 14:57:33 +0800

    add ignore

--- recognition/API_key.py
+++ recognition/API_key.py
@@ -4,3 +4,7 @@
 #                 'DeepSeek-R1-671B':[],
 #                 'deepseek-chat':[]，
 #                 'deepseek-reasoner':[]，}
+api_key_dict = {'silicon_flow_keys': ['sk-iewusjuafajsmfewuhzkzomedkmbrbmdcukxlvpocjrffroq'],
+                'DeepSeek-R1':['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI1NXEwRTVyS0N6WXZmV0Y2OHVlcm9SUnpJZEw0bnhHTCJ9.urxVYZO7wjx2tnG4OIgCFWwcA2VpgKLJr3lrV2C8shk'],
+                'DeepSeek-R1-671B':['eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb2RlIjoiMTAyNyIsImlhdCI6MTc0MjE4MDc5OCwiZXhwIjoxNzQyMjAyMzk4fQ.7goY655QKj3yZiLTMabEKV01ylZ5LBWpImVuXzWOenY'],
+                'deepseek-chat':['sk-e9ed51f02e494b329e4a75a255ffda0e']}
