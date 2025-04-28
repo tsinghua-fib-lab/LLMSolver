bb18cc68c32226625bc873ba1128dd31c80ed84b
qingwen778 <qwp1881051@163.com>
2025-04-28 21:09:28 +0800

    Update run.py

--- solver/bpp/run.py
+++ solver/bpp/run.py
@@ -2,14 +2,14 @@ from bpp_solver import genetic_algorithm
 import os
 import random
 
-random.seed(20)
+#random.seed(20)
 def generate_random_list(n):
     return [[random.randint(1, 10) for _ in range(3)] for _ in range(n)]
 dimension="2D"       # input:"2D" or "3D"
 is_online=0          # input:0 or 1
 can_rotate=0         # inut:1 or 0
 bin_size =[10,10]           # input:bin_size--[10,10] or [10, 10,10]
-box_sizes= generate_random_list(20)          # input:item_size--
+box_sizes= generate_random_list(20)          # input:item_size--n×2  or  n×3
 if dimension=="2D":
     bin_size=[10,1,10]
     box_sizes=[[row[0], 1, row[1]] for row in box_sizes]
