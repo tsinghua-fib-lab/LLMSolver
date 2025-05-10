7a52027779bf8027d99ca57e8d5c0b97d10d96fb
Qingwen Peng <qwp1881051@163.com>
2025-05-10 23:19:00 +0800

    Update kpdesc_generate.py

--- benchmark_hard/kp/kpdesc_generate.py
+++ benchmark_hard/kp/kpdesc_generate.py
@@ -21,10 +21,10 @@ with open("benchmark/knapsack/prompt.json", "r", encoding="utf-8") as f:
 with open("benchmark/knapsack/seed.json", "r", encoding="utf-8") as f:
     data_seed_prompt = json.load(f)    
 def generate_uniform_numbers(n):
-     return [random.uniform(0, 1) for _ in range(n)]
+     return [round(random.uniform(0, 1), 4) for _ in range(n)]
 for key in ["knapsack"]:
     problem_type=key
-    num_requests = 1  # 定义你想调用 API 的次数
+    num_requests = 1  
     for i in range(num_requests-1,num_requests+5):      
         Sector_process=','.join(Sector)
         user_input = data_prompt[key]+data_seed_prompt[key]+"The scenario title for each generated example must not duplicate any in the following list, meaning each application scenario must be distinct and cannot be the same as or similar to the ones in the list below:"+Sector_process+" ,Note that the language style of different examples must vary, and it should not be apparent that they were asked by the same person."
@@ -33,29 +33,23 @@ for key in ["knapsack"]:
         payload = {
             "model": MODEL_NAME,
             "messages": [
-                {"role": "system", "content": "请按照我的要求生成不同的问题描述。"},
+                {"role": "system", "content": "Please generate different problem descriptions according to my requirements."},
                 {"role": "user", "content": user_input}
             ],
             "temperature": 1.3,
             "max_tokens": 8000
         }
-
         headers = {
             "Authorization": f"Bearer {API_KEY}",
             "Content-Type": "application/json"
         }
-
-        # 调用 API
         response = requests.post(API_URL, json=payload, headers=headers)
 
         if response.status_code == 200:
             result = response.json()["choices"][0]["message"]["content"]
             total_tokens = response.json().get("usage", {}).get("total_tokens", None)
-#            print(result)
             print(f"Total tokens used: {total_tokens}")
             resulttt = result.split('&&&')
-#            for index, result in enumerate(resulttt, start=1):
-#            print("resulttt:",resulttt)
             for  result in resulttt:
 #            result = result.replace("\u2019", "'")
                 match = re.search(r"##(.*?)##", result)
@@ -64,21 +58,15 @@ for key in ["knapsack"]:
                     print("null_exist;")
                 else:
                    Sector.append(title)
-
-
-# 删除双井号及其中的内容
                    text_without_title = re.sub(r"##.*?##", "", result).strip()
-
                    responses=[]
-                   responses.append(text_without_title)  # 保存每次的回答
+                   responses.append(text_without_title)  
                    responses = [response.replace("\n", "") for response in responses]
                    responses = responses[0]
- #           responses=str(responses)
-#						new_text = "6666新增的一条文本。"
-# 追加模式 ('a') 打开文件，并写入新的 JSON 行
-                   item_weight = generate_uniform_numbers(20)
-                   item_value  = generate_uniform_numbers(20)
-                   knapsack_capacity=5
+                   n_item=random.randint(50, 100)
+                   item_weight = generate_uniform_numbers(n_item)
+                   item_value  = generate_uniform_numbers(n_item)
+                   knapsack_capacity=random.randint(5, 20)
                    desc_merge=responses.replace('<item_weight>', str(item_weight))
                    desc_merge=desc_merge.replace('<item_value>', str(item_value))
                    desc_merge=desc_merge.replace('<knapsack_capacity>', str(knapsack_capacity))
@@ -95,26 +83,15 @@ for key in ["knapsack"]:
                        }
                    try:
                       with open(filepath2, "r") as file:
-                          data_list = json.load(file)  # 读取文件中的数据
-                   except (FileNotFoundError, json.JSONDecodeError):  # 文件不存在或为空时
+                          data_list = json.load(file)  
+                   except (FileNotFoundError, json.JSONDecodeError):  
                        data_list = []
-# 向列表中追加新数据
                    data_list.append(add_data)
 
-# 将更新后的数据写回 JSON 文件
                    with open(filepath2, "w") as file:
-                      json.dump(data_list, file, indent=4)  #
+                      json.dump(data_list, file, indent=4)  
             
-#            if not os.path.exists("benchmark/description_generate/description/cvrp.json"):
-#                     with open("benchmark/description_generate/description/cvrp.json", "w", encoding="utf-8") as f:
-#                       json.dump([], f, ensure_ascii=False)
-#            with open("benchmark/description_generate/description/cvrp.json", "a", encoding="utf-8") as f:
-#                 json.dump({"label": "cvrp", "texts": add_data}, f, ensure_ascii=False)
-#                 f.write("\n")  # 确保每条数据单独一行
-            # 将结果写入文本文件
-#            file.write(f"{result}\n\n")
-            print(f"调用 {i + 1} 成功，结果已写入文件")
+            print(f"Call {i + 1} successful, the result has been written to the file.")
         else:
-#            file.write(f"调用 {i + 1} 失败，错误代码: {response.status_code}\n\n")
-            print(f"调用 {i + 1} 失败，错误代码: {response.status_code}")
-        print(Sector)
+            print(f"Call {i + 1} failed, error code: {response.status_code}")
+#        print(Sector)
